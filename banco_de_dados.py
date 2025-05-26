d4e3e58307bb1d829869da9344747d9e923ac985
Talison Azzini <talison.azzini@gmail.com>
2025-05-26 19:23:41 -0300

    Criação da função para criar tabela 'morador'

--- banco_de_dados.py
+++ banco_de_dados.py
@@ -20,11 +20,31 @@ class Banco_de_dados:
                     CREATE TABLE IF NOT EXISTS administracao(
                         id INTEGER PRIMARY KEY AUTOINCREMENT,
                         nome TEXT NOT NULL,
+                        telefone TEXT UNIQUE NOT NULL,
                         login TEXT UNIQUE NOT NULL,
                         senha TEXT NOT NULL,
-                        tipo TEXT NOT NULL CHECK (tipo IN ('porteiro', 'sindico'))
+                        tipo TEXT NOT NULL CHECK (tipo IN ('porteiro', 'sindico')),
+                        ativo INTEGER DEFAULT 1
                     )"""
                 )
                 self.conn.commit()
             except sqlite3.Error as e:
-                print(f"Erro ao criat tabela Administracao: {e}")
+                print(f"Erro ao criar tabela Administracao: {e}")
+
+    def tabela_morador(self):
+        if self.conn:
+            try:
+                cursor = self.conn.cursor()
+                cursor.execute("""
+                    CREATE TABLE IF NOT EXISTS morador(
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        nome TEXT NOT NULL,
+                        telefone TEXT UNIQUE NOT NULL,
+                        bloco TEXT NOT NULL,
+                        apartamento TEXT NOT NULL,
+                        ativo INTEGER DEFAULT 1
+                    )"""
+                )
+                self.conn.commit()
+            except sqlite3.Error as e:
+                print(f"Erro ao criar tabela Morador: {e}")
