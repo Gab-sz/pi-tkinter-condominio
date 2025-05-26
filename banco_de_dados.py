8f8effbca35004580a976799d663f1c05170bab4
Talison Azzini <talison.azzini@gmail.com>
2025-05-26 19:13:27 -0300

    Função para criar tabela 'administracao'

--- banco_de_dados.py
+++ banco_de_dados.py
@@ -12,4 +12,19 @@ class Banco_de_dados:
         except sqlite3.Error as e:
             print(f"Erro ao conectar ao banco de dados: {e}")
 
-    def criar
+    def tabela_administracao(self):
+        if self.conn:
+            try:
+                cursor = self.conn.cursor()
+                cursor.execute("""
+                    CREATE TABLE IF NOT EXISTS administracao(
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        nome TEXT NOT NULL,
+                        login TEXT UNIQUE NOT NULL,
+                        senha TEXT NOT NULL,
+                        tipo TEXT NOT NULL CHECK (tipo IN ('porteiro', 'sindico'))
+                    )"""
+                )
+                self.conn.commit()
+            except sqlite3.Error as e:
+                print(f"Erro ao criat tabela Administracao: {e}")
