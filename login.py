8d29c21391e52253391a521a2c766d59b37f5825
Gabriel Luciano <gbriells983@gmail.com>
2025-05-27 15:52:49 -0300

    FUNÇÃO JANELA

--- login.py
+++ login.py
@@ -6,32 +6,37 @@ def fazer_login():
     administrador = entrada_adm.get()
     senha = entrada_senha.get()
 
-    if administrador == "ADM.COND.123" and senha == "12345678":
+    if administrador == "ADM.123" and senha == "adm12345678":
         messagebox.showinfo("LOGIN", "LOGIN BEM-SUCEDIDO!")
     else:
         messagebox.showerror("ERRO", "ADMINISTRADOR OU SENHA INCORRETOS")
 
 # Criando a janela
-interface = tk.Tk()
-interface.title("TELA DE LOGIN")
-interface.geometry("500x300")
+def criar_janela():
+    global entrada_adm, entrada_senha
 
-# Título
-tk.Label(interface, text="LOGIN DO SISTEMA", font=("Arial",14)).pack(pady=10)
+    janela = tk.Tk()
+    janela.title("TELA DE LOGIN")
+    janela.geometry("500x300")
 
-# Campo Administrador
-tk.Label(interface, text="ADMINISTRADOR:").pack()
-entrada_adm = tk.Entry(interface)
-entrada_adm.pack()
+    # Título
+    tk.Label(janela, text="LOGIN DO SISTEMA", font=("Arial",14)).pack(pady=10)
 
-# Campo Senha
-tk.Label(interface, text="Senha:").pack()
-entrada_senha = tk.Entry(interface, show="*")
-entrada_senha.pack()
+    # Campo Administrador
+    tk.Label(janela, text="ADMINISTRADOR:").pack()
+    entrada_adm = tk.Entry(janela)
+    entrada_adm.pack()
 
-# Botão
-tk.Button(interface, text="Entrar", command=fazer_login).pack(pady=10)
+    # Campo Senha
+    tk.Label(janela, text="Senha:").pack()
+    entrada_senha = tk.Entry(janela, show="*")
+    entrada_senha.pack()
 
-# Loop da janela
-interface.mainloop()
+    # Botão
+    tk.Button(janela, text="Entrar", command=fazer_login).pack(pady=10)
 
+    # Loop da janela
+    janela.mainloop()
+
+# Chamar a função para iniciar a janela
+criar_janela()
